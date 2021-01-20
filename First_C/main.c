d60aba3e3253b2683ea89ce93968633e5240841d
nschaffin <nsc5181@psu.edu>
2021-01-19 22:12:51 -0500

    Initial Commit

---
+++ First_C/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  First_C
+//
+//  Created by Noah  Chaffin on 1/19/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
